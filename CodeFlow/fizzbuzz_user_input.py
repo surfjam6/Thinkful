2231481ca22d9d1718d843a5cd707d7e10178ed5
Jamie Weisbrod <weisbrod1@cox.net>
2016-03-04 05:25:16 +0000

    initial commit

---
+++ CodeFlow/fizzbuzz_user_input.py
@@ -0,0 +1,29 @@
+# fizzbuzz.py
+import sys
+
+min = 0
+maximum = 100
+
+if len(sys.argv) > 1:
+    max = int(sys.argv[1])
+else:
+    max = -1
+
+if max < 0:
+    try:
+        max = int(input("Enter max range\n"))
+    except ValueError:
+        max = maximum
+
+print("Fizz buzz counting up to {0}".format(max))
+print("\n")
+
+for number in range(min,max+1):
+    if (number % 3 == 0) and (number % 5 ==0):
+        print("FizzBuzz")
+    elif (number % 3 == 0):
+        print("Fizz")
+    elif (number % 5 == 0):
+        print("Buzz")
+    else:
+        print(number)
