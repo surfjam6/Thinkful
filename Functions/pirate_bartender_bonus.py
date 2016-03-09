21e8cfe00ce6cc977dd40b499a8e70cff9c81c9f
Jamie Weisbrod <weisbrod1@cox.net>
2016-03-09 23:46:11 +0000

    Added bonus features...drink name, check for y/n response

---
+++ Functions/pirate_bartender_bonus.py
@@ -0,0 +1,58 @@
+# pirate_bartender.py
+
+import random
+
+def drink_preferences(questions):
+  preferences = {}
+  """Asks customer preferences and created preferences dictionary"""
+  for taste in questions:
+    print(questions[taste])
+
+    while True:
+      answer = str.lower(input("Please enter your response: (y/n)"))
+      if answer in "YyNn":
+        break
+      else:
+        print("""Incorrect entry, please enter "y" or "n".""")
+    if answer == "y":
+      preferences.update({taste: True})
+  return preferences
+      
+def construct_drink(preferences, ingredients):
+  """Constructs an returns a drink"""
+  drink = []
+  for taste in preferences:
+    drink.append(random.choice(ingredients[taste]))
+  return drink
+
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
+
+adjective_list = ["Sporty","Fast", "Noisy", "Fluffy", "Squishy", "Green", "Blue", "Icy", "Chilled", "Spotted", "Snowy"]
+nouns_list = ["Owl", "Dog", "Lizard", "Hurricane", "Twister", "Slurpy", "Hawk", "Mouse"]
+
+def main():
+  preferences = drink_preferences(questions)
+  drink = construct_drink(preferences, ingredients)
+  print("\nI have created a drink according to your preferences. It will contain:")
+  for ingredient in drink:
+    print("A {}".format(ingredient))
+    
+  print("""Your drink is called a "{}, {}"!!""".format(random.choice(adjective_list), random.choice(nouns_list)))
+  
+if __name__ == "__main__":
+  main()
