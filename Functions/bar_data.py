cd857e4e1be99e87553a96a1d3e73c62bfb9076f
Jamie Weisbrod <weisbrod1@cox.net>
2016-03-19 18:21:18 +0000

    refractored to modules

---
+++ Functions/bar_data.py
@@ -0,0 +1,29 @@
+supply_count = 10
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
+
+inventory = {
+    "glug of rum": supply_count, "slug of whisky": supply_count, "splash of gin": supply_count,
+    "olive on a stick": supply_count, "salt-dusted rim": supply_count, "rasher of bacon": supply_count,
+    "shake of bitters": supply_count, "splash of tonic": supply_count, "twist of lemon peel": supply_count,
+    "sugar cube": supply_count, "spoonful of honey": supply_count, "spash of cola": supply_count,
+    "slice of orange": supply_count, "dash of cassis": supply_count, "cherry on top": supply_count
+}
+
+adjective_list = ["Sporty","Fast", "Noisy", "Fluffy", "Squishy", "Green", "Blue", "Icy", "Chilled", "Spotted", "Snowy"]
+nouns_list = ["Owl", "Dog", "Lizard", "Hurricane", "Twister", "Slurpy", "Hawk", "Mouse"]
+customer_preferences_list = {}
