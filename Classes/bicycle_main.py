528d215bb7efc72cc22b2f2dbfb8d67ce614c057
Jamie Weisbrod <weisbrod1@cox.net>
2016-03-21 21:18:01 +0000

    initial commit

---
+++ Classes/bicycle_main.py
@@ -0,0 +1,27 @@
+# biycle_main/py
+
+import bicycles
+import bike_inventory
+
+customer_list = []
+def main():
+    while True:
+        JoesBicycleStore = bicycles.Bike_Shop("""Joe's Bicycle Store""", bike_inventory.bicycle_inventory, .20)
+        Customer_1 = bicycles.Customer("John Smith", 200.00, "yes")
+        customer_list.append(Customer_1) 
+        Customer_2 = bicycles.Customer("Jane Appleton", 500.00, "yes")
+        customer_list.append(Customer_2) 
+        Customer_3 = bicycles.Customer("Byron Greene", 1000.00, "yes")
+        customer_list.append(Customer_3) 
+        print(JoesBicycleStore.name)
+        print(JoesBicycleStore.inventory)
+        print(JoesBicycleStore.inventory["mountain_bike"])
+        print(JoesBicycleStore.margin)
+        for i in range(len(customer_list)):
+            print("Customer Name: {}, Budget: ${}".format(customer_list[i].name, customer_list[i].budget))
+        break
+        
+
+
+if __name__ == "__main__":
+  main()
